1e7c044410f1b3c8045bcc876cbc2488524dcb7f
DTSmth <ducktapedave85@gmail.com>
2025-09-30 11:53:16 -0400

    Add methods to rectangle struct

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,15 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
 
     let rect1 = Rectangle {
@@ -11,11 +20,7 @@ fn main() {
         height: 50,
     };
 
-    println!("The area of the rectangle is {} square pixels.", area(&rect1));
-
-    fn area (rectangle: &Rectangle) -> u32 {
-        rectangle.width * rectangle.height
-    }
+    println!("The area of the rectangle is {} square pixels.", rect1.area());
 
     println!("rect1 is {rect1:?}")
 }
